38e18483771eb2552956b8aea66488d59eaaa90e
ricarthlima <ricarth.lima@gmail.com>
2025-09-03 20:11:19 -0300

    feat(spell): dataclass criado

---
+++ lib/domain/dto/spell.dart
@@ -0,0 +1,141 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class Spell {
+  int id;
+  String energy;
+  String name;
+  String verbal;
+  String description;
+  String actions;
+  String range;
+  List<String> tags;
+  bool isBond;
+  bool isResisted;
+  bool hasBaseTest;
+  List<String> actionIds;
+
+  Spell({
+    required this.id,
+    required this.energy,
+    required this.name,
+    required this.verbal,
+    required this.description,
+    required this.actions,
+    required this.range,
+    required this.tags,
+    required this.isBond,
+    required this.isResisted,
+    required this.hasBaseTest,
+    required this.actionIds,
+  });
+
+  Spell copyWith({
+    int? id,
+    String? energy,
+    String? name,
+    String? verbal,
+    String? description,
+    String? actions,
+    String? range,
+    List<String>? tags,
+    bool? isBond,
+    bool? isResisted,
+    bool? hasBaseTest,
+    List<String>? actionIds,
+  }) {
+    return Spell(
+      id: id ?? this.id,
+      energy: energy ?? this.energy,
+      name: name ?? this.name,
+      verbal: verbal ?? this.verbal,
+      description: description ?? this.description,
+      actions: actions ?? this.actions,
+      range: range ?? this.range,
+      tags: tags ?? this.tags,
+      isBond: isBond ?? this.isBond,
+      isResisted: isResisted ?? this.isResisted,
+      hasBaseTest: hasBaseTest ?? this.hasBaseTest,
+      actionIds: actionIds ?? this.actionIds,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'energy': energy,
+      'name': name,
+      'verbal': verbal,
+      'description': description,
+      'actions': actions,
+      'range': range,
+      'tags': tags,
+      'isBond': isBond,
+      'isResisted': isResisted,
+      'hasBaseTest': hasBaseTest,
+      'actionIds': actionIds,
+    };
+  }
+
+  factory Spell.fromMap(Map<String, dynamic> map) {
+    return Spell(
+      id: map['id']?.toInt() ?? 0,
+      energy: map['energy'] ?? '',
+      name: map['name'] ?? '',
+      verbal: map['verbal'] ?? '',
+      description: map['description'] ?? '',
+      actions: map['actions'] ?? '',
+      range: map['range'] ?? '',
+      tags: List<String>.from(map['tags']),
+      isBond: map['isBond'] ?? false,
+      isResisted: map['isResisted'] ?? false,
+      hasBaseTest: map['hasBaseTest'] ?? false,
+      actionIds: List<String>.from(map['actionIds']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Spell.fromJson(String source) => Spell.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Spell(id: $id, energy: $energy, name: $name, verbal: $verbal, description: $description, actions: $actions, range: $range, tags: $tags, isBond: $isBond, isResisted: $isResisted, hasBaseTest: $hasBaseTest, actionIds: $actionIds)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Spell &&
+        other.id == id &&
+        other.energy == energy &&
+        other.name == name &&
+        other.verbal == verbal &&
+        other.description == description &&
+        other.actions == actions &&
+        other.range == range &&
+        listEquals(other.tags, tags) &&
+        other.isBond == isBond &&
+        other.isResisted == isResisted &&
+        other.hasBaseTest == hasBaseTest &&
+        listEquals(other.actionIds, actionIds);
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        energy.hashCode ^
+        name.hashCode ^
+        verbal.hashCode ^
+        description.hashCode ^
+        actions.hashCode ^
+        range.hashCode ^
+        tags.hashCode ^
+        isBond.hashCode ^
+        isResisted.hashCode ^
+        hasBaseTest.hashCode ^
+        actionIds.hashCode;
+  }
+}
