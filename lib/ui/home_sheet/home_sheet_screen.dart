bde3ee6ed38e1c5eebf72d058ce3d5725d86d100
ricarthlima <ricarth.lima@gmail.com>
2025-09-17 00:51:33 -0300

    feat(home): agora fichas aparecem por botoes na home

--- lib/ui/home_sheet/home_sheet_screen.dart
+++ lib/ui/home_sheet/home_sheet_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rpg_audiodrama/ui/_core/dimensions.dart';
+// import 'package:flutter_rpg_audiodrama/ui/_core/dimensions.dart';
 import 'package:provider/provider.dart';
 
 import '../../_core/providers/user_provider.dart';
@@ -14,6 +14,7 @@ class HomeSheetScreen extends StatefulWidget {
 
 class _HomeSheetScreenState extends State<HomeSheetScreen> {
   final PageController pageController = PageController();
+  bool isMine = true;
 
   @override
   void initState() {
@@ -26,34 +27,90 @@ class _HomeSheetScreenState extends State<HomeSheetScreen> {
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context);
-
-    if (isVertical(context)) {
-      return PageView(
-        controller: pageController,
-        children: _buildBody(userProvider),
-      );
-    } else {
-      return Row(children: _buildBody(userProvider));
-    }
+    return Column(
+      children: [
+        Row(
+          spacing: 16,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isMine = true;
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: !isMine
+                    ? WidgetStatePropertyAll(
+                        Theme.of(context).scaffoldBackgroundColor,
+                      )
+                    : null,
+              ),
+              child: Text("Privados"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isMine = false;
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: isMine
+                    ? WidgetStatePropertyAll(
+                        Theme.of(context).scaffoldBackgroundColor,
+                      )
+                    : null,
+              ),
+              child: Text("Em campanhas"),
+            ),
+          ],
+        ),
+        Flexible(
+          child: AnimatedSwitcher(
+            duration: Duration(milliseconds: 300),
+            child: (isMine)
+                ? HomeListSheetsWidget(
+                    key: ValueKey("mine"),
+                    title: "Meus personagens",
+                    listSheets: userProvider.listSheetsOutCampaigns,
+                    username: userProvider.currentAppUser.username!,
+                  )
+                : HomeListSheetsWidget(
+                    key: ValueKey("other"),
+                    title: "Personagens de campanhas",
+                    listSheets: userProvider.listSheetsInCampaigns,
+                    username: userProvider.currentAppUser.username!,
+                    showAdding: false,
+                  ),
+          ),
+        ),
+      ],
+    );
+    // if (isVertical(context)) {
+    //   return PageView(children: _buildBody(userProvider));
+    // } else {
+    //   return Row(children: _buildBody(userProvider));
+    // }
   }
 
-  List<Widget> _buildBody(UserProvider userProvider) {
-    return [
-      Flexible(
-        child: HomeListSheetsWidget(
-          title: "Personagens avulsos",
-          listSheets: userProvider.listSheetsOutCampaigns,
-          username: userProvider.currentAppUser.username!,
-        ),
-      ),
-      Flexible(
-        child: HomeListSheetsWidget(
-          title: "Personagens em Campanhas",
-          listSheets: userProvider.listSheetsInCampaigns,
-          username: userProvider.currentAppUser.username!,
-          showAdding: false,
-        ),
-      ),
-    ];
-  }
+  // List<Widget> _buildBody(UserProvider userProvider) {
+  //   return [
+  //     Flexible(
+  //       child: HomeListSheetsWidget(
+  //         title: "Personagens avulsos",
+  //         listSheets: userProvider.listSheetsOutCampaigns,
+  //         username: userProvider.currentAppUser.username!,
+  //       ),
+  //     ),
+  //     Flexible(
+  //       child: HomeListSheetsWidget(
+  //         title: "Personagens em Campanhas",
+  //         listSheets: userProvider.listSheetsInCampaigns,
+  //         username: userProvider.currentAppUser.username!,
+  //         showAdding: false,
+  //       ),
+  //     ),
+  //   ];
+  // }
 }
